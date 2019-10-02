c97f875b960876b3f08e80831f84718a9a28c558
Alex Kolchak <flixlovemyfox@gmail.com>
2019-10-02 13:35:39 +0300

    BenarySerach version 2

--- src/Array.java
+++ src/Array.java
@@ -1,34 +1,26 @@
 public class Array {
-    public static int main (String[] args) {
+    public static void main(String[] args) {
         int[] sortedNumbers = {1, 2, 4, 15, 23, 36, 48, 59, 93};
-        int firstIndex  = 0;
+        int searchNumber = 2;
+
+        boolean i = true;
+        int firstIndex = 0;
         int lastIndex = sortedNumbers.length - 1;
-        int desiredNumber = 2;
+        while (i) {
+            int midIndex = (firstIndex + lastIndex) / 2;
 
-        while (firstIndex < lastIndex) {
-            int midLength = (firstIndex + lastIndex) / 2;
-            if (sortedNumbers[midLength] == desiredNumber){
-                return midLength; // элемент найден.
-            } else if (firstIndex > lastIndex) {
-                return lastIndex;
-            }
-            else if (sortedNumbers[midLength] < desiredNumber) {
-                    firstIndex = midLength + 1; // В верхней половине
+            if (sortedNumbers[midIndex] == searchNumber) {
+                System.out.println(midIndex);
+                i = false;
+            } else {
+                if (sortedNumbers[midIndex] < searchNumber) {
+                    firstIndex = midIndex + 1;
+                } else {
+                    if (sortedNumbers[midIndex] > searchNumber) {
+                        lastIndex = midIndex - 1;
+                    }
                 }
-            else if (sortedNumbers[midLength] > desiredNumber) {
-                lastIndex = midLength - 1; // В нижней половине
-            }
-
-        }
-        return firstIndex;
-    }
-
-    private static int getIndex(int[] arr, int var) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == var) { //arr[i] - возвращает элемент массива под индексом i
-                return i;
             }
         }
-        return -1;
     }
 }
